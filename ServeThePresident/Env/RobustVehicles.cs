2866629c792b2e495009ae2ed452e4b0355d30ec
Emre Cürgül <pongo1999712@gmail.com>
2018-05-30 00:03:30 +0200

    Enough progress for today

---
+++ ServeThePresident/Env/RobustVehicles.cs
@@ -0,0 +1,24 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using ServeThePresident.Util;
+using System.Threading.Tasks;
+
+namespace ServeThePresident.Env
+{
+    class RobustVehicles : BaseScript
+    {
+        public RobustVehicles()
+        {
+            Tick += OnTick;
+        }
+
+        private async Task OnTick()
+        {
+            await Delay(500);
+
+            if (Util.Player.SpawnedIn)
+                foreach (Vehicle vehicle in EntityEnum.GetAllVehicles())
+                    API.SetVehicleExplodesOnHighExplosionDamage(vehicle.Handle, false);
+        }
+    }
+}
