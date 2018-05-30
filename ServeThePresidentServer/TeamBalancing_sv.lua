e56a05c04aff8e93cb9254c6468704548ff26a30
IceHax <vecchiotom1@gmail.com>
2018-05-30 23:07:07 +0200

    added first variables to the serverside

--- ServeThePresidentServer/TeamBalancing_sv.lua
+++ ServeThePresidentServer/TeamBalancing_sv.lua
@@ -1,1 +1,13 @@
--- TODO
+-- 666 is a placeholder for no players
+President = 666
+VicePresident = 666
+--[[
+    SAMPLE STRUCTURE:
+    {
+        [ID] = {"steam:xxxxxx","ip:xxxxxxxxxxxx","license:xxxxxxxxxxxxxx"}
+    }
+]]--
+
+BodyGuards = {}
+Civilians = {}
+Terrorists = {}
