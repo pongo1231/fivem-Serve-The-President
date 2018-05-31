42882183216bec2bdc085c66203f350bf869f62e
Emre Cürgül <pongo1999712@gmail.com>
2018-05-31 23:59:55 +0200

    more push

---
+++ player/teamshandler.lua
@@ -0,0 +1,31 @@
+Citizen.CreateThread(function()
+	NetworkSetFriendlyFireOption(false)
+
+	while true do
+		Wait(100)
+
+		if PlayerPedId() then
+			for i = 0, 32, 1 do
+				if NetworkIsPlayerConnected(i) then
+					local playerPed = GetPlayerPed(i)
+					if playerPed then
+						local team = DecorGetInt(playerPed, "_PLAYER_TEAM")
+						local relationship = TeamRelationships[CurrentTeam.Get()][team]
+
+						SetPedAsEnemy(playerPed, relationship == TeamRelationshipType.Hostile)
+
+						local blip = GetBlipFromEntity(playerPed)
+						if relationship == TeamRelationshipType.Hostile and not team == TeamId.President and blip then
+							RemoveBlip(blip)
+						else
+							if not blip then
+								blip = AddBlipForEntity(playerPed)
+							end
+							SetBlipColour(blip, TeamBlipColors[team])
+						end
+					end
+				end
+			end
+		end
+	end
+end)
