b85cdcd47780d0c933e00db2b60a309f792ca555
IceHax <vecchiotom1@gmail.com>
2018-06-01 22:44:15 +0200

    added small first test for rounds logic

---
+++ server/roundlogic.lua
@@ -0,0 +1,11 @@
+local live = false
+counter = nil
+
+CreateThread(function()
+    while true do
+        Wait(0)
+        if not live then
+            --start round here (WIP)
+        end
+    end
+end)
